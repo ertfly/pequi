9481cd3e6130ede2f8239d1b0cb9a2a3d155eb8a
Eric Teixeira <ertfly@gmail.com>
2022-04-22 23:15:08 -0300

    ajustes

---
+++ src/PSQLEntity.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Pequi;
+
+use Medoo\Medoo;
+
+abstract class DatabaseEntity
+{
+    abstract public function setId($id);
+    abstract public function getId();
+    abstract public function toArray();
+
+    public function insert(Medoo $db, $isId = false)
+    {
+        $tabela = static::TABLE;
+        $db->insert($tabela, $this->toArray());
+        if (!$isId) {
+            $id = $db->id();
+            $this->setId($id);
+            return;
+        }
+        return;
+    }
+
+    public function save(Medoo $db, $where = [])
+    {
+        $tabela = static::TABLE;
+        if (!$this->getId()) {
+            $db->insert($tabela, $this->toArray());
+            $id = $db->id();
+            $this->setId($id);
+            return;
+        }
+
+        if(count($where)==0){
+            $where['id'] = $this->getId();
+        }
+        $db->update($tabela, $this->toArray(), $where);
+    }
+
+    public function delete(Medoo $db, $where = [])
+    {
+        if(count($where)==0){
+            $where['id'] = $this->getId();
+        }
+        $tabela = static::TABLE;
+        if (!$this->getId()) {
+            return;
+        }
+        $db->update($tabela, ['trash' => true], $where);
+    }
+
+    public function destroy(Medoo $db, $where = [])
+    {
+        if(count($where)==0){
+            $where['id'] = $this->getId();
+        }
+        $tabela = static::TABLE;
+        if (!$this->getId()) {
+            return;
+        }
+        $db->delete($tabela, $where);
+    }
+
+    public function fromJson(array $json)
+    {
+        foreach ($json as $campo => $valor) {
+            $c = $campo;
+            $this->$c = $valor;
+        }
+    }
+}
